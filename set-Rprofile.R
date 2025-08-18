232c9943a953b8fa0d5a509b1167195c6d052952
tbarthelemy <tanguy.barthelemy@insee.fr>
2025-08-18 10:16:50 +0000

    add .Rprofile

---
+++ set-Rprofile.R
@@ -0,0 +1,31 @@
+# Configuration environment ----------------------------------------------------
+
+lines_to_add <- c(
+    '
+options(
+    warnPartialMatchAttr = TRUE,
+    warnPartialMatchDollar = TRUE,
+    warnPartialMatchArgs = TRUE,
+    showErrorCalls = TRUE
+)
+
+options(useFancyQuotes = FALSE)
+
+invisible(Sys.setlocale("LC_ALL", "en_GB.UTF-8"))
+
+sunrise <- bioRad::sunrise(date = Sys.Date(), lon = 2.3, lat = 48.8)
+sunset <- bioRad::sunset(date = Sys.Date(), lon = 2.3, lat = 48.8)
+
+setHook("rstudio.sessionInit", function(newSession) {
+    if (newSession) {
+        if (Sys.time() < sunrise || Sys.time() > sunset) {
+            rstudioapi::applyTheme("Material")
+        } else {
+            rstudioapi::applyTheme("Textmate (default)")
+        }
+    }
+}, action = "append")
+    '
+)
+
+write(lines_to_add, file = file.path(Sys.getenv("HOME"), ".Rprofile"), append = TRUE)
