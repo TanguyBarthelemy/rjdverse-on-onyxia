72a6a2be0ab670993c2d664e2cf7f4d4460c6b1d
tbarthelemy <tanguy.barthelemy@insee.fr>
2025-08-18 09:58:52 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Initialize the rjdverse on Onyxia
+
+[![Onyxia](https://img.shields.io/badge/Launch-Datalab-orange?logo=R)](https://datalab.sspcloud.fr/launcher/ide/rstudio?version=2.3.12&autoLaunch=true&onyxia.friendlyName=%C2%ABDevelop-rjdverse%C2%BB&init.personalInit=«https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/refs/heads/main/init.sh»&vault.secret=«PAT»)
---
+++ init.sh
@@ -0,0 +1,18 @@
+git config --global credential.helper "cache --timeout=10000000"
+
+git clone https://github.com/TanguyBarthelemy/rjd3toolkit.git
+git clone https://github.com/TanguyBarthelemy/rjdemetra.git
+git clone https://github.com/TanguyBarthelemy/rjd3bench.git
+git clone https://github.com/TanguyBarthelemy/rjd3providers.git
+git clone https://github.com/TanguyBarthelemy/rjd3revisions.git
+git clone https://github.com/TanguyBarthelemy/rjd3nowcasting.git
+git clone https://github.com/TanguyBarthelemy/rjd3x13.git
+git clone https://github.com/TanguyBarthelemy/rjd3workspace.git
+git clone https://github.com/TanguyBarthelemy/rjd3tramoseats.git
+git clone https://github.com/TanguyBarthelemy/rjd3sts.git
+git clone https://github.com/TanguyBarthelemy/rjd3filters.git
+git clone https://github.com/TanguyBarthelemy/rjd3highfreq.git
+git clone https://github.com/TanguyBarthelemy/rjd3stl.git
+git clone https://github.com/TanguyBarthelemy/rjd3x11plus.git
+
+Rscript "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/refs/heads/main/install.R"
