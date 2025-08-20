15fe30fa82e76d07d5c7a3a6a0e9687e60618c35
Tanguy Barthelemy <tanguy.barthelemy@insee.fr>
2025-08-20 15:24:58 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,19 +3,19 @@ git config --global user.email tanguy.barthelemy@insee.fr
 git config --global credential.username TanguyBarthelemy
 
 git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3toolkit.git
-#git clone https://github.com/TanguyBarthelemy/rjdemetra.git
-#git clone https://github.com/TanguyBarthelemy/rjd3bench.git
-#git clone https://github.com/TanguyBarthelemy/rjd3providers.git
-#git clone https://github.com/TanguyBarthelemy/rjd3revisions.git
-#git clone https://github.com/TanguyBarthelemy/rjd3nowcasting.git
-#git clone https://github.com/TanguyBarthelemy/rjd3x13.git
-#git clone https://github.com/TanguyBarthelemy/rjd3workspace.git
-#git clone https://github.com/TanguyBarthelemy/rjd3tramoseats.git
-#git clone https://github.com/TanguyBarthelemy/rjd3sts.git
-#git clone https://github.com/TanguyBarthelemy/rjd3filters.git
-#git clone https://github.com/TanguyBarthelemy/rjd3highfreq.git
-#git clone https://github.com/TanguyBarthelemy/rjd3stl.git
-#git clone https://github.com/TanguyBarthelemy/rjd3x11plus.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjdemetra.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3bench.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3providers.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3revisions.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3nowcasting.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3x13.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3workspace.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3tramoseats.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3sts.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3filters.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3highfreq.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3stl.git
+git clone https://TanguyBarthelemy:${GITHUB_TANGUYBARTHELEMY}@github.com/TanguyBarthelemy/rjd3x11plus.git
 
 curl -o "${HOME}/config.R" 'https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/refs/heads/main/set-Rprofile.R'
 curl -o "${HOME}/install.R" 'https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/refs/heads/main/install.R'
