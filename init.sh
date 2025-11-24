efa11d663278e76dd2b6b7b9c91a1c618d0277a9
Tanguy Barthelemy <tanguy.barthelemy@insee.fr>
2025-11-24 16:17:16 +0100

    v1.13

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ mkdir -p "${init_dir}"
 chown -R onyxia:users "${init_dir}"
 
 cd ${init_dir}
-echo "v1.12"
+echo "v1.13"
 
 curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-git-TanguyBarthelemy.sh"
 curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-rstudio-prefs.sh"
@@ -14,3 +14,17 @@ curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-on
 curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-rprofile.sh"
 curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-air.sh"
 curl -fsSL -O "https://raw.githubusercontent.com/TanguyBarthelemy/rjdverse-on-onyxia/main/library/setup-firacode.sh"
+
+chmod +x "setup-git-TanguyBarthelemy.sh"
+chmod +x "setup-rstudio-prefs.sh"
+chmod +x "setup-locales.sh"
+chmod +x "setup-rprofile.sh"
+chmod +x "setup-air.sh"
+chmod +x "setup-firacode.sh"
+
+"./setup-git-TanguyBarthelemy.sh"
+"./setup-rstudio-prefs.sh"
+"./setup-locales.sh"
+"./setup-rprofile.sh"
+"./setup-air.sh"
+"./setup-firacode.sh"
