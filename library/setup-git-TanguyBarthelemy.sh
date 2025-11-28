7a376234e472655afa05260fff9b43217fb02167
Tanguy Barthelemy <tanguy.barthelemy@insee.fr>
2025-11-28 10:59:24 +0100

    Update setup-git-TanguyBarthelemy.sh

--- library/setup-git-TanguyBarthelemy.sh
+++ library/setup-git-TanguyBarthelemy.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 git config --global user.name "Tanguy BARTHELEMY"
 git config --global user.email tanguy.barthelemy@insee.fr
 git config --global credential.username TanguyBarthelemy
+git config --global credential.helper 'cache --timeout=604800'
+
 echo "GITHUB_PAT=\${GITHUB_TANGUYBARTHELEMY}" >> "${HOME}/.Renviron"
