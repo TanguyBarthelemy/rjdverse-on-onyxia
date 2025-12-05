c2fa0d52fa5ba89a91afbdae5e6128332a7bb493
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2025-12-05 17:13:51 +0000

    reset setup with all options

--- library/setup-rprofile.sh
+++ library/setup-rprofile.sh
@@ -2,10 +2,9 @@
 
 # Installer des packages supplémentaires
 Rscript -e "
-#install.packages(\"BiocManager\", repos = \"https://cloud.r-project.org\")
-#BiocManager::install(\"rhdf5\", ask = FALSE)
-#install.packages(c(\"bioRad\", \"rstudioapi\"), repos = \"https://cloud.r-project.org\")
-install.packages(\"rstudioapi\", repos = \"https://cloud.r-project.org\")
+install.packages(\"BiocManager\", repos = \"https://cloud.r-project.org\")
+BiocManager::install(\"rhdf5\", ask = FALSE)
+install.packages(c(\"bioRad\", \"rstudioapi\"), repos = \"https://cloud.r-project.org\")
 "
 
 # Créer un .Rprofile
@@ -30,22 +29,19 @@ options(useFancyQuotes = FALSE)
 
 invisible(Sys.setlocale(\"LC_ALL\", \"en_GB.UTF-8\"))
 
-#sunrise <- bioRad::sunrise(date = Sys.Date(), lon = 2.3, lat = 48.8)
-#sunset <- bioRad::sunset(date = Sys.Date(), lon = 2.3, lat = 48.8)
+sunrise <- bioRad::sunrise(date = Sys.Date(), lon = 2.3, lat = 48.8)
+sunset <- bioRad::sunset(date = Sys.Date(), lon = 2.3, lat = 48.8)
 
 setHook(\"rstudio.sessionInit\", function(newSession) {
     if (newSession) {
-        #if (Sys.time() < sunrise || Sys.time() > sunset) {
-        #    rstudioapi::applyTheme(\"Material\")
-        #} else {
-        #    rstudioapi::applyTheme(\"Textmate (default)\")
-        #}
+        if (Sys.time() < sunrise || Sys.time() > sunset) {
+            rstudioapi::applyTheme(\"Material\")
+        } else {
+            rstudioapi::applyTheme(\"Textmate (default)\")
+        }
 
         PROJ_DIR <- Sys.getenv(\"PROJ_DIR\")
         if (nzchar(PROJ_DIR)) PROJ_DIR <- normalizePath(PROJ_DIR)
-        print(PROJ_DIR)
-        print(getwd())
-        Sys.sleep(20)
 
         if (nzchar(PROJ_DIR) && !identical(PROJ_DIR, getwd())) {
             message(\"Activation du projet RStudio\")
